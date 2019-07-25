493336af43a457d8aff1ae23c405c40048914e1b
Julian Nadeau <jules2689@github.com>
2019-07-24 17:07:46 -0700

    Allow cd behaviour to be explicit via config

--- lib/d2/commands/cd.rb
+++ lib/d2/commands/cd.rb
@@ -5,22 +5,29 @@ module D2
   module Commands
     class Cd < D2::Command
       def call(args, _name)
-        # This find command will enumerate the src_path directory for all repos
-        # Assumes src_path/PROVIDER.com/OWNER/REPO
-        # Will return all `REPO` entries relative to the src path
-        base_path = File.expand_path(D2::Config.get('src_path', 'default'))
-        options = D2::Helpers::Fzy.fuzzy_match(
-          "find #{base_path}/*/* -type d -maxdepth 1 -mindepth 1 | sed -n 's|^#{base_path}||p'",
-          query: args.shift,
-          num_matches: 1
-        )
+        query = args.shift
 
-        if options.empty?
-          logger.info "Nothing found for #{args.first}"
+        path = if exact = D2::Config.get('cd', query)
+          exact
         else
-          path = File.join(base_path, options.first)
-          D2::FILE_DESCRIPTOR.write("cd #{path}")
+          # This find command will enumerate the src_path directory for all repos
+          # Assumes src_path/PROVIDER.com/OWNER/REPO
+          # Will return all `REPO` entries relative to the src path
+          base_path = File.expand_path(D2::Config.get('src_path', 'default'))
+          options = D2::Helpers::Fzy.fuzzy_match(
+            "find #{base_path}/*/* -type d -maxdepth 1 -mindepth 1 | sed -n 's|^#{base_path}||p'",
+            query: query,
+            num_matches: 1
+          )
+
+          if options.empty?
+            logger.info "Nothing found for #{args.first}"
+            return
+          end
+          File.join(base_path, options.first)
         end
+
+        D2::FILE_DESCRIPTOR.write("cd #{path}")
       end
 
       def self.help
