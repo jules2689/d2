eda0189878648c2dd5f6b052cc985865803ac551
Julian Nadeau <jules2689@github.com>
2019-08-09 18:37:35 -0400

    Change to allow config files to specify paths with {{config path}} expander, dont use temp file

--- lib/d2/commands/local_command.rb
+++ lib/d2/commands/local_command.rb
@@ -11,25 +11,27 @@ module D2
       def call(args, _name)
         help and return true if args.include?("--help")
 
-        require 'tempfile'
-        require 'fileutils'
+        # If the run command is one line, it may be a file to run
+        # Check if it is, if it is, run it.
+        command = nil
+        if @params['run'].lines.size == 1
+          path = @params['run'].gsub(/{{config}}/, D2::CONFIG_PATH)
+          if File.exist?(path)
+            command = path
+          end
+        end
+        command = @params['run'] if command.nil?
 
-        Tempfile.create("#{@name}", Dir.pwd) do |file|
-          file.write(@params['run'])
-          file.rewind
-          FileUtils.chmod("+x", file.path)
-
-          CLI::UI::Frame.open("Running #{@name} command") do
-            CLI::Kit::System.system(file.path, *args) do |o, e|
-              if o
-                o.split("{{divider}}").each_with_index do |section, idx|
-                  CLI::UI::Frame.divider(nil) unless idx == 0
-                  print CLI::UI.fmt section
-                end
+        CLI::UI::Frame.open("Running #{@name} command") do
+          CLI::Kit::System.system(command, *args) do |o, e|
+            if o
+              o.split("{{divider}}").each_with_index do |section, idx|
+                CLI::UI::Frame.divider(nil) unless idx == 0
+                print CLI::UI.fmt section
               end
-              print CLI::UI.fmt e if e
-              print CLI::UI::Color::RESET.code
             end
+            print CLI::UI.fmt e if e
+            print CLI::UI::Color::RESET.code
           end
         end
       end
